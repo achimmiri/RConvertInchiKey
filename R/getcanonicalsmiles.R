14e3ac0ec9152bb61e733abc9e2c48e398616825
Ahusha Chimmiri <achimmiri@ipb-halle.de>
2023-02-26 20:29:49 +0100

    this function converts inchikey to canonical smiles

---
+++ R/getcanonicalsmiles.R
@@ -0,0 +1,19 @@
+getcanonicalsmiles<-<-function(getINK)
+{
+  ###### This Functions return canonical smiles
+  url<- "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/inchikey/"
+  out<-tryCatch({jsonlite::fromJSON(paste0(url,getINK, "/JSON"))} ,error = function(x) {return(NA)})
+  prop.names  <-tryCatch({out$PC_Compounds$props[[1]][[1]]},error = function(x) {return(NA)})
+  prop.values <- tryCatch({out$PC_Compounds$props[[1]][[2]]},error = function(x) {return(NA)})
+  sm <-tryCatch({grep("smiles", prop.names[,"label"], ignore.case = TRUE)},error = function(x) {return(NA)})
+  csmiles<-c()
+  if(length(sm) >= 1 & !sjmisc::is_empty(sm)) {
+    can <- tryCatch({grep("canonical", prop.names[,"name"], ignore.case = TRUE)},error= function(x) {return(NA)})
+    can1<-tryCatch({prop.values[sm[1],"sval"]},warning= function(x) {return(NA)})
+    csmiles<-c(csmiles,can1)
+  }else{
+    csmiles<-c(csmiles,NA)
+  }
+  return(csmiles)
+  
+}
